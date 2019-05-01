7642c59127b49ae3c10318a78f7cd418d8469a93
Victor Nghe <victor.nghe@gmail.com>
2019-04-30 21:38:45 -0400

    feat: Add some more info to Home

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -5,7 +5,7 @@ const Home = () => (
     <h1>Hello, World! <span role="img" aria-label="">👋</span></h1>
     <section className="intro">
       My name is Victor Nghe. I&apos;m a{' '}
-      <a href="https://www.linkedin.com/in/victornghe">web developer</a> by
+      <a href="">web developer</a> by
       trade, but I dabble in <a href="https://victorcares.wordpress.com/">writing</a>{' '}
       from time to time.
     </section>
@@ -15,12 +15,19 @@ const Home = () => (
       <ul>
         <li>Italian <span role="img" aria-label="">🇮🇹</span></li>
         <li><a href="https://www.rust-lang.org/">Rust</a></li>
+        <li><a href="https://flutter.dev/">Flutter</a></li>
       </ul>
       <h3>Things I&apos;d like to see in my lifetime:</h3>
       <ul>
         <li>Affordable Healthcare</li>
         <li>Colonization of Mars</li>
         <li>Faster-than-light Travel</li>
+      </ul>
+      <h3>Links:</h3>
+      <ul>
+        <li><a href="https://www.linkedin.com/in/victornghe">LinkedIn</a></li>
+        <li><a href="https://blog.victornghe.com">Blog: Victor Explains Nothing New</a></li>
+        <li><a href="https://www.victorcares.com">Blog: Victor Cares</a></li>
       </ul>
     </section>
   </div>
